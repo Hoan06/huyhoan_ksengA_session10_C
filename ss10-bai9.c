8b76ac089f94d76244551a3ec6a1e114f5ca85c9
Hoan06 <167394101+Hoan06@users.noreply.github.com>
2024-12-03 17:26:22 +0700

    btvn_ss10_bai9

---
+++ ss10-bai9.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+	int a[3]={0};
+	int i,j,temp;
+	int array[3][3]={{1,4,7},{6,5,2},{8,9,3}};
+	int size=sizeof(array)/sizeof(int);
+	for (int i=0;i<3;i++){
+		a[i]=array[i][i];
+	}
+	for ( i=0;i<3;i++){
+		for ( j=0;j<3-i-1;j++){
+			if (a[j]>a[j+1]){				
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	for ( i=0;i<3;i++){
+		array[i][i]=a[i];
+	}
+	for (i=0;i<3;i++){
+		for(j=0;j<3;j++){
+		    printf("%d ",array[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
